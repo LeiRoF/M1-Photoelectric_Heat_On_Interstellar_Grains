c2c228cea70a85f32c589c8e57c452379c6a00bf
Leirof <vince.lrf@gmail.com>
2021-09-27 17:17:31 +0200

    Electrons can now be emitted! \o/

--- get_energies.py
+++ get_energies.py
@@ -26,9 +26,7 @@ def throwPhoton(grain, verbose = False):
 
     # Getting random distance using the probability P=exp(-da/la) / la
     la = 100 # 10^-6 cm = 100 angstrom -> 100 pixels
-    da = utils.randomInDistrib(lambda x: numpy.exp(-x/la) / la)
-    if verbose:
-        print("da = ", da)
+    da = utils.randomInDistrib(lambda x: numpy.exp(-x/la) / la,0,100)
 
     # Getting absorbtion position
     x = 0
@@ -45,14 +43,54 @@ def throwPhoton(grain, verbose = False):
     
     # Printing results
     if verbose:
-        print("Distance traveled in grain ", round(da,3), "angstrom")
-        print("Point of impact ", hit_pos)
-        print("Absorbed at column ", x)
+        print("   - A photon appeared in column ", start_pos, "!")
+        print("   - It have an energy of ", round(energy,3), " eV")
+        if hit_pos == -1:
+            print("❌ - But... it missed the grain")
+        else:
+            print("✅ - It hitted the grain at line ", start_pos, ", column ", hit_pos, "!")
+            print("   - In this condition, the photon will travel ", round(da,3), "angstroms trhough the grain")
+            if x == -1:
+                print("❌ - And... this distance was to hight, it passed trhough the grain :/")
+            else:
+                print("✅ - Oh, it was absorbed at column ", x)
 
     # Emit electron with probability Y=0.5*(1+Th[(E-E0)/2])
     if photonAbsorbed:
         E0 = 8
-        electron_emitted = utils.randomInDistrib(lambda x: 0.5*(1+numpy.tanh[(energy-E0)/2]))
+        electron_emitted = rand() < 0.5*(1+numpy.tanh((energy-E0)/2))
+        if verbose:
+            print("   - In this conditions, the probability of emitting an electron is ", round(0.5*(1+numpy.tanh((energy-E0)/2)),3))
+            if electron_emitted:
+                print("✅ - An electron has been emitted!")
+            else:
+                print("❌ - No electron was emitted :/")
+
+        if electron_emitted:
+            # Getting random distance traveld by the electron using the probability P=exp(-de/le) / le
+            le = 10 # 10^-6 cm = 100 angstrom -> 100 pixels
+            de = utils.randomInDistrib(lambda x: numpy.exp(-x/le) / le,0,100)
+            if verbose:
+                print("   - In this condition, the electron will travel ", round(de,3), "angstroms trhough the grain")
+
+            # Getting absorbtion position
+
+            distBeforeAbsorbtion = int(de) + 1
+            while x < len(grain) and distBeforeAbsorbtion > 0:
+                if int(grain1D[x]):
+                    distBeforeAbsorbtion -= 1
+                x += 1
+            if x == 100:
+                x = -1
+                electronAbsorbed = False
+            else:
+                electronAbsorbed = True
+            if verbose:
+                if electronAbsorbed:
+                    print("❌ - The electron was re-absorbed at column ", x, "!")
+                else:
+                    print("✅ - The electron was ejected!")
+    
 
 if __name__ == "__main__":
     grain = getGrainFromFile("grains/Grain_N100_S1p0_B3p0.txt")
--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     dict={}
     for i in range(10000):
         #r = randomInDistrib(lambda x: numpy.exp(-x/100)/100,0,50)               # P = exp(-da/la) / la
-        r = randomInDistrib(lambda x: 0.5*(1+numpy.tanh((x-8)/2)), 3, 15)       # Y = 0.5*(1+Th[(E-E0)/2])
+        r = randomInDistrib(lambda x: 0.5*(1+numpy.tanh((x-8)/2)), 0, 1000)       # Y = 0.5*(1+Th[(E-E0)/2])
         try:
             dict.update({int(r) : dict[int(r)]+1})
         except KeyError:
